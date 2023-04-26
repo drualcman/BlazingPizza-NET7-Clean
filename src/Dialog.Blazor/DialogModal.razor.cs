aee5689ecce98862695904417663e27d09ee9406
Sergi Ortiz Gómez <drualcman@msn.com>
2023-04-26 09:55:38 +0800

    creado un component para encapsular los modales

---
+++ src/Dialog.Blazor/DialogModal.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Dialog.Blazor;
+public partial class DialogModal
+{
+    [Parameter] public RenderFragment ChildContent { get; set; }
+    [Parameter, EditorRequired] public bool IsVisible { get; set; }
+
+}
