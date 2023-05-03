a01717b4d78e88ce8601425afd221205247c3fc8
Sergi Ortiz Gómez <drualcman@msn.com>
2023-05-03 12:16:52 +0800

    agregando un error persoanlizado para el acceso no autorizado

--- src/CustomExceptions.HttpHandlers/StatusCodes.cs
+++ src/CustomExceptions.HttpHandlers/StatusCodes.cs
@@ -5,4 +5,6 @@ internal class StatusCodes
     public const string Status400BadRequestType = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1";
     public const int Status500InternalServerError = 500;
     public const string Status500InternalServerErrorType = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1";
+    public const int Status401Unauthorized = 401;
+    public const string Status401UnauthorizedType = "https://datatracker.ietf.org/doc/html/rfc7235#section-3.1";
 }
---
+++ src/CustomExceptions.HttpHandlers/UnauthorizedAccessExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomExceptions.HttpHandlers;
+internal class UnauthorizedAccessExceptionHandler : IHttpExceptionHandler<UnauthorizedAccessException>
+{    
+    public ProblemDetails Handle(UnauthorizedAccessException exception) 
+    {
+
+        ProblemDetails problemDetails = new()
+        {
+            Status = StatusCodes.Status401Unauthorized,
+            Type = StatusCodes.Status401UnauthorizedType,
+            Title = "Acceso no autorizado.", 
+            Detail = "El recurso solicitado no fue autorizado.",
+            InvalidParams = null,
+        };
+        return problemDetails;
+    }
+}
