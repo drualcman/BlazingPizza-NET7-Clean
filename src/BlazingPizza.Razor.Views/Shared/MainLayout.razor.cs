b93902188ed700fd145f2ae570b7c9ac7d95642b
Sergi Ortiz Gómez <drualcman@msn.com>
2023-09-17 06:41:43 +0800

    detectar un 401 y redireccionar a login.

--- src/BlazingPizza.Razor.Views/Shared/MainLayout.razor.cs
+++ src/BlazingPizza.Razor.Views/Shared/MainLayout.razor.cs
@@ -4,10 +4,19 @@ namespace BlazingPizza.Razor.Views.Shared;
 
 public partial class MainLayout
 {
+    [Inject] NavigationManager navigationManager { get; set; }
+
     CustomErrorBoundary ErrorBoundaryRef;
     void OnExeption(Exception ex)
     {
         Console.WriteLine($"Error en Main Layout: {ex.Message}");
+        if(ex.Message.Contains("401"))
+        {
+            Task.Run(() =>
+            {                   
+                navigationManager.NavigateTo("user/login");
+            });
+        }
     }
 
     protected override void OnParametersSet()
